1c8166523dabac482d460f54b21e27905f897840
Mark Anthony <manthonyamaro@yahoo.com>
2023-05-23 13:47:12 -0500

    created next section of html for the search button to be utilized and css styling for it while setting up the event listener to set up upcoming functions for searching for the weather

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //Api Key from site
 const apiKey = '599e78c3459a8c73ce2b3faeb84e514d';
+
+document.getElementById('search-btn').addEventListener('click', function() {
+    const cityInput = document.getElementById('city-input').value;
+    searchWeather(cityInput);
+  });
+  
